9212f031b49149cd0d50cb7f6764e0538a3f6863
ivysonnn <ivysonlucas27@hotmail.com>
2026-04-06 17:07:18 -0300

    move the line/s up and down, ident keybind

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,9 +7,23 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-vim.keymap.set('n', '<leader>pv', ':Ex<cr>', { desc = 'Open [E]xplorer' })
-
 vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
 vim.keymap.set('n', '<leader>w', ':write<CR>')
 vim.keymap.set('n', '<leader>q', ':quit<CR>')
+
+
+-- Mover linhas
+vim.keymap.set("n", "<A-k>", "<cmd>move .-2<cr>==", { desc = "move line up" })
+vim.keymap.set("n", "<A-j>", "<cmd>move .+1<cr>==", { desc = "move line down" })
+vim.keymap.set("v", "<A-k>", ":move '<-2<cr>gv=gv", { desc = "move selection up" })
+vim.keymap.set("v", "<A-j>", ":move '>+1<cr>gv=gv", { desc = "move selection down" })
+
+-- Normal mode: reindenta a linha atual
+vim.keymap.set("n", "<leader>i", "==", { desc = "reindent line" })
+
+-- Visual mode: reindenta a seleção e mantém selecionado
+vim.keymap.set("v", "<leader>i", "=gv", { desc = "reindent selection" })
+
+-- Normal mode: reindenta o arquivo inteiro
+vim.keymap.set("n", "<leader>I", "gg=G", { desc = "reindent file" })
 
